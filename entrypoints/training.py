749f610b514637f59e6fdc166c516a5ebab5c130
halflearned <halflearned@gmail.com>
2025-12-03 19:39:46 +0000

    SFT on gsm8k running with weight sync by saving to disk -> loading -> trl-based communication; for qwen3-0.6B this takes 30s, likely unacceptable. but we'll eat this as first pass.

--- entrypoints/training.py
+++ entrypoints/training.py
@@ -21,52 +21,24 @@ def register_gsm8k():
 register_gsm8k()
 
 
-from trl.extras.vllm_client import VLLMClient
-print("Creating vLLM client...")
-client = VLLMClient(base_url="http://vllm:8000", connection_timeout=120)
-print("Calling vLLM...")
-output = client.generate(prompts=["Say hello from the RLVR trainer."])
-print("vLLM call succeeded.", output)
-
 
 
 # -------- trainer stuff
-from torchtitan.train import Trainer
 from torchtitan.tools.logging import init_logger
-from torchtitan.config import ConfigManager
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("config", type=str)
 args, _ = parser.parse_known_args()
+from trl.extras.vllm_client import VLLMClient
+client = VLLMClient(base_url="http://vllm:8000", connection_timeout=120)
 
 init_logger()
+
+from rlvr_experiments.torchtitan.train import Trainer as SyncingTrainer
+from torchtitan.config import ConfigManager
+#from torchtitan.train import Trainer
 config = ConfigManager().parse_args(["--job.config-file", args.config])
-trainer = Trainer(config)
-
-import torch
-import torch.distributed as dist
-import os
-
-rank = dist.get_rank()
-
-
-# 1) init communicator
-#Need to map using this adapter (or its reverse, actually):
-# https://github.com/pytorch/torchtitan/blob/b39377f9fe33865fefb9bf64a33f6d74a598be87/torchtitan/models/qwen3/model/state_dict_adapter.py#L28
-if rank == 0:
-    client.init_communicator(device=0) 
-    print("INITIALIZED COMMUNICATOR") 
-    name = "model.layers.0.self_attn.q_proj.weight"
-    dummy = torch.randn(2048, 1024).cuda(0)
-    client.update_named_param(name, dummy)
-    print("UPDATED MODEL PARAMS") 
-    client.close_communicator()
-    print("CLOSED COMMUNICATOR!")
-print("SUCCESS! Weight sync smoke test completed.")
-
-
-
-
-
+config.vllm_update_steps = 1
+trainer = SyncingTrainer(config, vllm_client=client)
 trainer.train()
